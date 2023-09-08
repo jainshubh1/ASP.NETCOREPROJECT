5b4e8467a0a14f35ebfc8a2acb9699399019377e
jainshubh1 <144414223+jainshubh1@users.noreply.github.com>
2023-09-08 12:01:33 +0530

    Add files via upload

---
+++ ClaimsPrincpalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace RunGroupWebApp
+{
+    public static class ClaimsPrincpalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.NameIdentifier).Value;
+        }
+    }
+}
